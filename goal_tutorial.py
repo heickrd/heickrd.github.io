e542b7297b5694c90374dacb627a9f55e3c908a4
heickrd <heickrd@dukes.jmu.edu>
2020-03-20 14:39:15 -0400

    Update goal_tutorial.py

--- goal_tutorial.py
+++ goal_tutorial.py
@@ -1,23 +1,8 @@
-# Copyright 2016 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import rclpy
 import rclpy.qos
 from rclpy.node import Node
-from geometry_msgs.msg import Twist, PoseStamped
+from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import LaserScan
-from std_msgs.msg import String
 from nav_msgs.msg import Odometry
 from time import sleep
 
@@ -26,15 +11,8 @@ class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        #print("Made it init")
-        #self.scan_sub = self.create_subscription(LaserScan, 'scan', self.scan_callback, qos_profile=rclpy.qos.qos_profile_sensor_data)
-        #print("After scan")
-      #  self.odom_sub = self.create_subscription(Odometry, 'odom', self.odom_callback, 10)
-     #   self.cmd_vel_pub = self.create_publisher(Twist, "cmd_vel", 10)
+
         self.goal_pub = self.create_publisher(PoseStamped, "move_base_simple/goal", 10)
-        vel = Twist()
-        vel.linear.x = 1.0
-        vel.angular.z = 1.0
         goal = PoseStamped()
         goal.header.frame_id = "odom"
         goal.pose.position.x = 11.7
@@ -54,17 +32,13 @@ class MinimalPublisher(Node):
             i = i -1
             goal.pose.position.x = i
 
-        print("After pub")
-
     
 
 def main(args=None):
     rclpy.init(args=args)
-    print("In main")
     minimal_publisher = MinimalPublisher()
 
     rclpy.spin(minimal_publisher)
-    print("After spin")
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
